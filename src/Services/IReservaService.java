dd9ee193585f7d4da1a702208f767fed92470ca7
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-01-08 16:37:02 +0100

    cambios

--- src/Services/IReservaService.java
+++ src/Services/IReservaService.java
@@ -9,7 +9,7 @@ import DTO.ReservaDTO;
 import DTO.VueloDTO;
 
 public interface IReservaService extends Remote{
-	List<VueloDTO> buscarVuelos(String origen, String destino, int nPlazas) throws RemoteException;
-	boolean realizarReserva(ReservaDTO reservaARealizar, int nPlazas, String[] pasajeros) throws RemoteException;
+	List<VueloDTO> buscarVuelos(String origen, String destino, int numeroplazas) throws RemoteException;
+	boolean realizarReserva(ReservaDTO reservarealizada, int numeroplazas) throws RemoteException;
 	List<ReservaDTO> getReservas(UsuarioDTO usuario) throws RemoteException;
 }
