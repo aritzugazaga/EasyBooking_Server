aefd8c568c23d3a6e5d980c540a493896ac2ba05
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-01-19 16:32:18 +0100

    cambios en los gateways por los servidores externos

--- src/Gateways/GatewayGoogle.java
+++ src/Gateways/GatewayGoogle.java
@@ -8,70 +8,29 @@ public class GatewayGoogle implements IGatewayLogin {
 	private String IP = "127.0.0.1";
 	private int puerto = 1099;
 	private String server = "Google";
+	private IGoogle google;
+	
+	
+	public GatewayGoogle() {
+		try {
+			String servidor = IP + "," + puerto + "," + server;
+			google = (IGoogle) java.rmi.Naming.lookup(servidor);
+		}catch(Exception e) {
+			System.err.println("Excepcion:  " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public boolean registrar(String email, String contrasenya) throws RemoteException {
 		// TODO Auto-generated method stub
-		boolean registro = false;
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + server;
-			IGoogle google = (IGoogle) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				registro = google.login(email, contrasenya);
-				if (registro) {
-					System.out.println("Los datos del usuario son correctos, usuario registrado");
-				}
-				else {
-					System.out.println("Los datos del usuario son incorrectos, usuario no registrado");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion: " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return registro;
+		return google.registrar(email, contrasenya);
+				
 	}
 	
-
 	@Override
 	public boolean login(String email, String contrasenya) throws RemoteException {
 		// TODO Auto-generated method stub
-		boolean login = false;
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + server;
-			IGoogle google = (IGoogle) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				login = google.login(email, contrasenya);
-				if (login) {
-					System.out.println("Los datos del usuario son correctos, login correcto");
-				}
-				else {
-					System.out.println("Los datos del usuario son incorrectos, login incorrecto");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion:  " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return login;
+		return google.login(email, contrasenya);
 	}
-
 }
--- src/Gateways/GatewayPaypal.java
+++ src/Gateways/GatewayPaypal.java
@@ -1,6 +1,7 @@
 package Gateways;
 
 import java.rmi.RemoteException;
+
 import server.remote.IPaypal;
 
 public class GatewayPaypal implements IGatewayPago {
@@ -8,69 +9,27 @@ public class GatewayPaypal implements IGatewayPago {
 	private String IP = "127.0.0.1";
 	private int puerto = 1099;
 	private String server = "Paypal";
+	private IPaypal paypal;
+	
+	public GatewayPaypal() {
+		try {
+			String servidor = IP + "," + puerto + "," + server;
+			paypal = (IPaypal) java.rmi.Naming.lookup(servidor);
+		}catch(Exception e) {
+			System.err.println("Excepcion: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public boolean pagar(int precio_unitario, String email, String codigo) throws RemoteException {
 		// TODO Auto-generated method stub
-		boolean pago = false;
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + server;
-			IPaypal paypal = (IPaypal) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				pago = paypal.pagar(precio_unitario, email, codigo);
-				if (pago) {
-					System.out.println("Los datos de pago son correctos");
-				}
-				else {
-					System.out.println("Los datos de pago son incorrectos");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion: " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return pago;
-	}
-
+		return paypal.pagar(precio_unitario, email, codigo);		
+	}	
+	
 	@Override
 	public boolean tienedinero(int precio_unitario, String email, String codigo) throws RemoteException {
 		// TODO Auto-generated method stub
-		boolean tienedinero = false;
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + server;
-			IPaypal paypal = (IPaypal) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				tienedinero = paypal.tienedinero(precio_unitario, email, codigo);
-				if (tienedinero) {
-					System.out.println("Hay fondos para pagar");
-				}
-				else {
-					System.out.println("No hay fondos suficientes para pagar");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion: " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return tienedinero;
+		return paypal.tienedinero(precio_unitario, email, codigo);
 	}
-
 }
