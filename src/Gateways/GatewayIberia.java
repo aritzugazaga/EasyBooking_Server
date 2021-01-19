72b5b97385359e06f83430982b024fef9a18b5c3
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-01-19 18:36:11 +0100

    servicio externo aerolinea iberia

--- src/Gateways/GatewayIberia.java
+++ src/Gateways/GatewayIberia.java
@@ -5,74 +5,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 import DTO.VueloDTO;
+import server.remote.IIberia;
 
 public class GatewayIberia implements IGatewayAerolinea {
 	
-	private String IP;
-	private int puerto;
-	private String nombreserver;
+	private String IP = "127.0.0.1";
+	private int puerto = 1099;
+	private String server = "Iberia";
+	private IIberia iberia;
+	
+	public GatewayIberia() {
+		try {
+			String servidor = IP + "," + puerto + "," + server;
+			iberia = (IIberia) java.rmi.Naming.lookup(servidor);
+		}catch(Exception e) {
+			System.err.println("Excepcion: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public List<VueloDTO> busquedavuelo(String aeropuertodestino, String aeropuertoorigen) throws RemoteException {
 		// TODO Auto-generated method stub
-		List<VueloDTO> vuelos = new ArrayList<VueloDTO>();
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
+		ArrayList<VueloDTO> a = new ArrayList<VueloDTO>();
+		for(server.data.dto.VueloDTO v : iberia.busquedavuelo(aeropuertoorigen, aeropuertodestino)) {
+			a.add(cambiodto(v));
 		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + nombreserver;
-			IGatewayAerolinea server = (IGatewayAerolinea) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				vuelos = server.busquedavuelo(aeropuertodestino, aeropuertoorigen);
-				if (vuelos != null) {
-					System.out.println("Los datos de busqueda de vuelo son correctos");
-				}
-				else {
-					System.out.println("Los datos de busqueda de vuelo son incorrectos");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion: " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return vuelos;
+		return a;
 	}
 
 	@Override
 	public boolean reservavuelo(String codigovuelo, String nombre, int plazas) throws RemoteException {
 		// TODO Auto-generated method stub
-		boolean reserva = false;
-
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
+		return iberia.reservavuelo(codigovuelo, nombre, plazas);
+	}
+	
+	private VueloDTO cambiodto(server.data.dto.VueloDTO vr) throws RemoteException  {
+		if (vr != null) {
+			VueloDTO v = new VueloDTO();
+			v.setAeropuertodestino(vr.getAeropuertodestino());
+			v.setAeropuertoorigen(vr.getAeropuertoorigen());
+			v.setFecha_salida(vr.getFecha_salida());
+			v.setAsientos_disponibles(vr.getAsientos_disponibles());
+			v.setCodigovuelo(vr.getCodigovuelo());
+			v.setPrecio_unitario(60);
+			return v;
+		} else {
+		return null;
 		}
-
-		try {
-			String servidor = IP + "," + puerto + "," + nombreserver;
-			IGatewayAerolinea server = (IGatewayAerolinea) java.rmi.Naming.lookup(servidor);
-			
-			try {
-				reserva = server.reservavuelo(codigovuelo, nombre, plazas);
-				if (reserva) {
-					System.out.println("Los datos de reserva de vuelo son correctos");
-				}
-				else {
-					System.out.println("Los datos de reserva de vuelo son incorrectos");
-				}
-			} catch (Exception e) {
-				System.err.println("Excepcion: " + e.getMessage());
-				e.printStackTrace();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
- 
-		return reserva;
 	}
 }
